8f5c9eeb082d6d9e638443ba64fbec4a54cf3af8
zhangjsf <zhangjsf@enn.cn>
2017-11-01 17:39:23 +0800

    remove files

--- src/main/java/cn/mapway/wiki/servers/FileServer.java
+++ src/main/java/cn/mapway/wiki/servers/FileServer.java
@@ -3,18 +3,13 @@ package cn.mapway.wiki.servers;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
-import org.nutz.json.Json;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.stereotype.Component;
 
-import com.google.gwt.cell.client.FieldUpdater;
-
+import cn.mapway.common.servlets.files.FileUploadServlet;
 import cn.mapway.wiki.configure.FileUploadProperties;
-import cn.mapway.wiki.servlet.FileUploadServlet;
+
 
 /**
  * 文件服务器.
@@ -27,7 +22,7 @@ public class FileServer extends ServletRegistrationBean {
 
 	FileUploadProperties properties;
 
-	@Autowired
+	@Autowired 
 	public FileServer(FileUploadProperties p) {
 		properties = p;
 		FileUploadServlet servlet = new FileUploadServlet();
